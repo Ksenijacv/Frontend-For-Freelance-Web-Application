c5ac3bcd044da379a2efc9cbb54a0ae42f907168
Ksenija Cvjetan <ksenija.cvjetan@gmail.com>
2024-01-16 22:40:58 +0100

    Kreirana pocetna login staticka stranica koja nakon uspesnog logovanja vodi na pocetnu stranicu. Uradjen i css i ubacena ruta u App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route,Routes, Navigate  } from 'react-router-dom';
+import LoginForm from './components/LoginForm.jsx';
+import React, { useState } from 'react';
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogin = (username) => {
+    setLoggedInUser(username);
+  };
+
+  const handleLogout = () => {
+    setLoggedInUser(null);
+    return <Navigate to="/" />;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      
+        <Routes>
+          <Route
+            path="/"
+            element={loggedInUser ? <Navigate to="/home" /> : <LoginForm onLogin={handleLogin} />}
+          />
+
+
+        </Routes>
+      </BrowserRouter>
+    
     </div>
   );
 }
